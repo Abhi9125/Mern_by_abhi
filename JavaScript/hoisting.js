c55f766b86c67fd2b1cabca83dbd10da7867d22a
Abhi9125 <abhi.singh3132@gmail.com>
2022-10-21 02:53:56 +0530

    hoisting and GEC in js

--- JavaScript/hoisting.js
+++ JavaScript/hoisting.js
@@ -1,12 +1,20 @@
 // JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, variables to the top of their scope, prior to execution of the code.
 
-console.log(a);
-fun();
-var a = 10;
-function fun() {
-  console.log("FJP-9 rocks");
-}
-console.log(a);
+
+// console.log(a);
+// var ans = fun();
+// console.log(ans);
+// var a = 10;
+// function fun() {
+//   // console.log("FJP-9 rocks");
+//   return "FJP-9 rocks";
+// }
+// console.log(greet);
+// var greet = function () {
+//   console.log("hello");
+// }
+// console.log(greet);
+// console.log(a);
 
 // var a;
 // function fun() {
@@ -18,5 +26,12 @@ console.log(a);
 // a = 10;
 // console.log(a);
 
-//my definition by abhishek bhaiya 
+//my definition
 // acccesing of variables and functions even before they are declared
+
+//---------------Temporal dead zone--------------------
+console.log(a);
+let a = 10;
+cosole.log(b);
+var b = 100;
+console.log(x);

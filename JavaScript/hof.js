928dcbc8111fbec851d63cacfe43922093ca51c8
Abhi9125 <abhi.singh3132@gmail.com>
2023-04-03 14:45:20 +0530

    hof

--- JavaScript/hof.js
+++ JavaScript/hof.js
@@ -139,3 +139,118 @@ function greaterthanaNumber(num){
 }
 var ans = arr.filter(greaterthanaNumber);
 console.log(ans);
+
+
+
+// reduce-----------------------------------------
+var arr = [2,5,7,12];
+
+function sumOfArr(acc,cur) {
+    console.log("Before");
+    console.log(acc);
+    console.log(cur);
+    acc = acc + cur;
+    console.log("After");
+    console.log(acc);
+    return acc;
+}
+
+// let ans = arr.reduce(sumOfArr); // in this call acc by defaul arr[0] ki value liga ar curr arr[1] se start ho.
+var ans = arr.reduce(sumOfArr,0); // in this call acc me 0 value liga ar curr arr[0] se start ho
+console.log(ans);
+
+
+// Q
+var arr = [18, -5, 566, 28, 56, 12, 456];
+
+function largestValue(acc,curr){
+if(curr > acc){
+    acc = curr;
+} 
+return acc;
+}
+
+let ans1 = arr.reduce(largestValue);
+console.log(ans1); 
+
+// Interview Q on map,filter,reduce
+
+const worldCapital = [
+{
+    country : "India",
+    capital : "Delhi",
+    population : "2cr", 
+},
+{
+    country : "America",
+    capital : "Washington DC",
+    population : "6cr", 
+},
+{
+    country : "France",
+    capital : "Paris",
+    population : "2cr", 
+},
+{
+    country : "England",
+    capital : "London",
+    population : "4cr", 
+},
+{
+    country : "Germany",
+    capital : "Berlin",
+    population : "2cr", 
+},
+];
+
+//Q1 print country name and their capital  
+// [ India->Delhi, America->WashingtonDC, France->Paris , England->London, Germany->Berlin] 
+function getCountryCapital(obj){
+    return (obj.country +"-->"+ obj.capital);
+}
+var res = worldCapital.map(getCountryCapital);
+console.log(res);
+
+//Q2-> Return the number of countries with a particular population 
+//{ 2cr: 3, 4cr: 1, 6cr: 1 }
+// use reduce bcz hme ans object e chahiye 
+function getPerticularPopu(obj,cur){
+    if(obj[cur.population]){  // use [] for object
+        obj[cur.population] = obj[cur.population]+1;
+    }else{
+        obj[cur.population] = 1;
+    }
+    return obj;
+}
+
+var res2 = worldCapital.reduce(getPerticularPopu,{});
+console.log(res2);
+
+
+//Q3 get country name with population less than 5cr
+//[India, France,England,Germany]
+
+function getPopulationLessThan5cr(obj) {
+    if (obj.population < "5cr") {
+      return true;
+    }
+  }
+  function getCountry(obj) {
+    return obj.country;
+  }
+  var res = worldCapitals.filter(getPopulationLessThan5cr).map(getCountry);
+  console.log(res);
+  
+  //using reduce 
+  
+  function getPopulationLessThan5cr(arr, obj) {
+    if (obj.population < "5cr") {
+      arr.push(obj.country);
+    }
+    return arr;
+  }
+  
+  var ans = worldCapitals.reduce(getPopulationLessThan5cr,[]);
+  console.log(ans);
+
+
